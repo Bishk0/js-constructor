a84582b73132615e8e60daa6dba2fe181be44816
Bishko <yurik322412@gmail.com>
2020-08-21 11:20:48 +0300

    Optimizing and improving the code

--- src/model.js
+++ src/model.js
@@ -1,7 +1,14 @@
 export const model = [
-    {type: 'title', value: 'Test Title', options: {}},
-    {type: 'text', value: 'Test Text', options: {}},
+    {type: 'title', value: 'Test Title', options: {
+        tag: 'h2',
+        styles: 'background: darkred; color: #fff;'
+        }},
+    {type: 'text', value: 'Test Text', options: {
+            styles: 'background: darkblue; color: #fff;'
+        }},
     {type: 'textColumns', value: [
         '1text', '2text', '3text'
-        ], options: {}},
+        ], options: {
+            styles: 'padding: 1rem;'
+        }},
 ];
