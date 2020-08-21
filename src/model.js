c52be2125552976d3d1a08e1bcb5dd3b11cee7ca
Bishko <yurik322412@gmail.com>
2020-08-21 10:33:53 +0300

    Building a JavaScript model

---
+++ src/model.js
@@ -0,0 +1,7 @@
+export const model = [
+    {type: 'title', value: 'Test Title', options: {}},
+    {type: 'text', value: 'Test Text', options: {}},
+    {type: 'textColumns', value: [
+        '1text', '2text', '3text'
+        ], options: {}},
+];
