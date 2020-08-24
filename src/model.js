0de3e6b71973bc1188886517cc39a312b6f84e45
Bishko <yurik322412@gmail.com>
2020-08-24 18:18:56 +0300

    OOP and SOLID

--- src/model.js
+++ src/model.js
@@ -1,14 +1,28 @@
+import img from './assets/tumblr_ph7j2aNzQe1s49f4m_1280.jpg';
+import {TitleBlock, TextBlock, ColumnBlock, ImageBlock} from './classes/blocks'
+
 export const model = [
-    {type: 'title', value: 'Test Title', options: {
+    new TitleBlock( 'Constructor web sites', {
         tag: 'h2',
         styles: 'background: darkred; color: #fff;'
-        }},
-    {type: 'text', value: 'Test Text', options: {
-            styles: 'background: darkblue; color: #fff;'
-        }},
-    {type: 'textColumns', value: [
-        '1text', '2text', '3text'
-        ], options: {
-            styles: 'padding: 1rem;'
-        }},
+    }),
+    new TextBlock('Aperiam asperiores beatae corporis dicta dolorem, dolores eius exercitationem expedita illum ipsum maxime minus\n' +
+        '    molestiae, molestias nesciunt nobis non odit possimus, voluptate. Beatae iste magnam necessitatibus officia quo!\n' +
+        '    Amet, saepe.', {
+        styles: 'background: darkblue; color: #fff;'
+    }),
+    new ColumnBlock(['Aperiam asperiores beatae corporis dicta dolorem, dolores eius exercitationem expedita illum ipsum maxime minus\n' +
+    '    molestiae, molestias nesciunt nobis non odit possimus, voluptate. Beatae iste magnam necessitatibus officia quo!\n' +
+    '    Amet, saepe.', 'Aperiam asperiores beatae corporis dicta dolorem, dolores eius exercitationem expedita illum ipsum maxime minus\n' +
+    '    molestiae, molestias nesciunt nobis non odit possimus, voluptate. Beatae iste magnam necessitatibus officia quo!\n' +
+    '    Amet, saepe.', 'Aperiam asperiores beatae corporis dicta dolorem, dolores eius exercitationem expedita illum ipsum maxime minus\n' +
+    '    molestiae, molestias nesciunt nobis non odit possimus, voluptate. Beatae iste magnam necessitatibus officia quo!\n' +
+    '    Amet, saepe.'], {
+        styles: 'padding: 1rem;'
+    }),
+    new ImageBlock(img, {
+        styles: 'padding: 2rem 0; display: flex; justify-content: center;',
+        alt: 'image',
+        imageStyles: 'width: 500px; height: auto;'
+    }),
 ];
